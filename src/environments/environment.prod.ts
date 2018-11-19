b336d3810200e8cac61bb7334df0d047933427a2
cuongrio07 <cuongrio07@gmail.com>
2018-11-19 11:17:36 +0700

    delete

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  version: '1.0.0',
-  serverUrl: 'https://api.chucknorris.io',
-  defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'fr-FR']
-};
